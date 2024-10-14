7bf7dead3ed46b8d5622f71038357341aacc76e6
Victor Correa <victor1cs@outlook.com>
2024-10-14 09:50:31 -0300

    Creating validation in the model and adding some information in the view

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,11 @@
 class Tournament < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :buy_in, presence: true
+
+  def balance
+    return unless !award.blank?
+    award - buy_in
+  end
 end
